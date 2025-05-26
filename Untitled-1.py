81ebb2a8796108117aa01e50118e534301b107fa
JESUS <marcos.jesus.py25@gmail.com>
2025-05-26 17:42:49 -0300

    senhas!!

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+
+
+
+senha = input("Digite a  senha : ")
+
+while senha != "123456" :
+  senha = input("digite a senha correta")
+if senha == "123456":
+         print("senha correta")
+else : 
+       
+ print("senha incorreta ! tenta novamente.")
